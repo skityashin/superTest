d61043c26785ef21ea9ea6877bc71c4a0050b500
Skityashin <skityashin@gmail.com>
2017-01-19 11:34:35 +0200

    4

--- src/main/java/com/skityashin/Application.java
+++ src/main/java/com/skityashin/Application.java
@@ -1,22 +1,22 @@
-package com.skityashin;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-
-
-/**
- * Class {@link Application}
- *
- * @author Skityashin Vladimir
- * @version 1.0
- * @since 18.01.17
- */
-
-@SpringBootApplication
-@ComponentScan({"com.skityashin"})
-public class Application {
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
-}
+//package com.skityashin;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.context.annotation.ComponentScan;
+//
+//
+///**
+// * Class {@link Application}
+// *
+// * @author Skityashin Vladimir
+// * @version 1.0
+// * @since 18.01.17
+// */
+//
+//@SpringBootApplication
+//@ComponentScan({"com.skityashin"})
+//public class Application {
+//    public static void main(String[] args) {
+//        SpringApplication.run(Application.class, args);
+//    }
+//}
